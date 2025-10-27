cb4323bbd63fcc1cd27689a21e7fc975cc9abc68
vaibhav-js <vaibhav.js@samsung.com>
2025-10-27 21:00:21 +0530

    Resolve incidents

--- src/index.js
+++ src/index.js
@@ -4,15 +4,6 @@ async function acknowledgeIncidents() {
       Authorization: `Token token=${process.env.PAGERDUTY_API_KEY}`,
       'Content-Type': 'application/json',
     };
-    console.log(process.env);
-    let s=JSON.parse(process.env);
-    console.log(s);
-    console.log(s.PAGERDUTY_API_KEY);
-    console.log(process.env);
-    console.log(process.env.RESOLVABLE_PAGERS);
-    let pages= JSON.parse(process.env)
-    console.log("🚀 ~ acknowledgeIncidents ~ pages:", pages)
-    console.log(typeof(process.env.RESOLVABLE_PAGERS));
     const url = `https://api.pagerduty.com/incidents?statuses[]=triggered&user_ids[]=${process.env.PAGERDUTY_USER_ID}`;
     
     const response = await fetch(url, { headers });
@@ -24,29 +15,26 @@ async function acknowledgeIncidents() {
     const incidents = data.incidents;
 
     if (!incidents || incidents.length === 0) {
-      console.log('No triggered incidents found for the user.');
       return;
     }
 
-    console.log(`Found ${incidents.length} triggered incidents.`);
     
     const promises = incidents.map((incident) => {
       const incidentId = incident.id;
-      const ackUrl = `https://api.pagerduty.com/incidents/${incidentId}`;
-      const ackBody = JSON.stringify({
+      const url = `https://api.pagerduty.com/incidents/${incidentId}`;
+      const body = JSON.stringify({
         incident: {
           type: 'incident_reference',
-          status: process.env.RESOLVABLE_PAGERS.includes(incident.title) ? 'acknowledged':'resolved',
+          status: 'resolved', //'acknowledged'
         },
       });
 
-      return fetch(ackUrl, {
+      return fetch(url, {
         method: 'PUT',
         headers,
-        body: ackBody,
+        body: body,
       }).then((ackResponse) => {
         if (ackResponse.ok) {
-          console.log(`Acknowledged incident: ${incidentId}`);
         } else {
           console.error(`Failed to acknowledge incident: ${incidentId}. Status: ${ackResponse.status}`);
         }
@@ -55,7 +43,6 @@ async function acknowledgeIncidents() {
 
     await Promise.all(promises);
 
-    console.log('All incidents processed.');
   } catch (error) {
     console.error('Error:', error.message);
     process.exit(1);
